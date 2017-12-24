655c82d145baaaafed68830bf0efadff2764472e
Jared Pleva <jpleva91@gmail.com>
2017-12-24 16:04:03 -0700

    working on access control

--- server/main.js
+++ server/main.js
@@ -11,6 +11,11 @@ Meteor.startup(() => {
   });
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/api/motivations', (req, res) => {
   const data = Motivations.find().fetch();
