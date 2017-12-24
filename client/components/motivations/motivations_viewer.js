5c3d15b7d828039207cace9b10d18ab0d95250c0
Jared Pleva <jpleva91@gmail.com>
2017-12-24 11:26:27 -0700

    wires up motivation viewer to server publisher

--- client/components/motivations/motivations_viewer.js
+++ client/components/motivations/motivations_viewer.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Motivations } from '../../../imports/collections/motivations';
 
 class MotivationViewer extends Component {
   render() {
+    if(!this.props.motivation) { return <div>Loading...</div>};
+    
     return (
       <div>
         MotivationViewer
@@ -10,4 +14,9 @@ class MotivationViewer extends Component {
   }
 }
 
-export default MotivationViewer;
+export default createContainer((props) => {
+  const { motivationId } = props.params;
+  Meteor.subscribe('motivations');
+
+  return { motivation: Motivations.findOne(motivationId) };
+}, MotivationViewer);
--- server/main.js
+++ server/main.js
@@ -2,5 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Motivations } from '../imports/collections/motivations';
 
 Meteor.startup(() => {
-
+  Meteor.publish('motivations', function() {
+    return Motivations.find({});
+  });
 });
