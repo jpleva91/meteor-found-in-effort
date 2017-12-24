e4b87ac11e90bc418bc77532ae8cb66fd79259b4
Jared Pleva <jpleva91@gmail.com>
2017-12-24 13:02:18 -0700

    updates motivations editor

--- client/components/motivations/motivations_editor.js
+++ client/components/motivations/motivations_editor.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 
 class MotivationsEditor extends Component {
-  onContentChange(content) {
-    console.log(content);
+  constructor(props) {
+    super(props);
+
+    this.state={
+      content: this.props.motivation.content,
+      author: this.props.motivation.author
+    };
   }
 
-  onAuthorChange(content) {
-    console.log(content);
+  onContentChange(event) {
+    this.setState({ content: event.target.value });
+  }
+
+  onAuthorChange(event) {
+    this.setState({ author: event.target.value });
+  }
+
+  onButtonSave() {
+    console.log(`content: ${this.state.content}`);
+    console.log(`author: ${this.state.author}`);
+
+    Meteor.call('motivations.update', this.props.motivation, this.state.content, this.state.author);
   }
   
   render() {
@@ -14,12 +30,28 @@ class MotivationsEditor extends Component {
       <div className="form-group col-xs-8">
         <div>
           <label>Content</label>
-          <textarea className="form-control" rows="5" id="content"></textarea>
+          <textarea 
+            value={this.state.content}
+            onChange={this.onContentChange.bind(this)}
+            className="form-control" 
+            rows="5" 
+            id="content">
+          </textarea>
           <label>Author</label>
-          <textarea className="form-control" rows="2" id="author"></textarea>
+          <textarea
+            value={this.state.author}
+            onChange={this.onAuthorChange.bind(this)} 
+            className="form-control" 
+            rows="1" 
+            id="author">
+          </textarea>
         </div>
         <span>
-          <button className="btn btn-space btn-success">Save</button>
+          <button 
+            onClick={this.onButtonSave.bind(this)}
+            className="btn btn-space btn-success">
+              Save
+          </button>
         </span>
       </div>
     );
