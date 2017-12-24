c25077cf95522c0ff803dc8f8dea243c00d51495
Jared Pleva <jpleva91@gmail.com>
2017-12-24 15:46:56 -0700

    adds api to backend server

--- server/main.js
+++ server/main.js
@@ -1,8 +1,20 @@
 import { Meteor } from 'meteor/meteor';
+import express from 'express';
 import { Motivations } from '../imports/collections/motivations';
+import  { WebApp } from 'meteor/webapp';
+
+const app = express();
 
 Meteor.startup(() => {
   Meteor.publish('motivations', function() {
     return Motivations.find({});
   });
 });
+
+
+app.get('/api/motivations', (req, res) => {
+  const data = Motivations.find().fetch();
+  res.status(200).json({ data });
+});
+
+WebApp.connectHandlers.use(app);
